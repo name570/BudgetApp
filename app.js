ad94a6cf2ac10170b1772f697661b4e9aba4aff3
name570 <nikittara@yandex.ru>
2019-07-31 17:48:58 +0300

    add module logic and some test functions with public and private

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("asasa")
+ var budgetController = (function (){
+    var x = 23;
+
+    var add = function(a){
+        return x + a;
+    }
+    
+    return {
+        publicTest: function(b){
+            return add(b);
+        }
+    } 
+ })();
+
+  var UIController = (function(){
+      //TODO
+  })();
+
+
+  var controller = (function(budgetCtlr, UICtrl){
+
+    var z = budgetCtlr.publicTest(5);
+
+    return {
+        anotherPublic: function(){
+            console.log(z);
+        }
+    }
+
+  })(budgetController,UIController);
