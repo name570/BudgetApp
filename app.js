0fe02af015bb11ca4c9fe3705e4d5e6c3529c0ac
name570 <nikittara@yandex.ru>
2019-08-01 20:02:04 +0300

    adding reading data from UIController and some handler with this data in controller module

--- app.js
+++ app.js
@@ -1,30 +1,58 @@
+ 
+ // BUDGET CONTROLLER
  var budgetController = (function (){
-    var x = 23;
-
-    var add = function(a){
-        return x + a;
-    }
-    
-    return {
-        publicTest: function(b){
-            return add(b);
-        }
-    } 
+    //TO DO
  })();
 
-  var UIController = (function(){
-      //TODO
+// UI Controller
+ var UIController = (function(){
+     var DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+
+     };
+      return {
+          getInput: function(){
+
+            return {
+                type: document.querySelector(DOMStrings.inputType).value, // Either inc or exp
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value 
+            };     
+          },
+          getDOMStrings: function(){
+              return DOMStrings; 
+          }
+      };
   })();
 
 
+  //GLOBAL APP CONTROLLER
   var controller = (function(budgetCtlr, UICtrl){
+    
+    var DOM =  UICtrl.getDOMStrings();
 
-    var z = budgetCtlr.publicTest(5);
+    var ctrlAddItem = function() {
+        // TO DO LIST
+        // 1. Get the field input data
+        var input = UICtrl.getInput();
+        console.log(input);
+        // 2. Add the item to the budget controller
+        // 3.  Add new item to UI
+        // 4. Calculate the budget
+        // 5. Display the budget on the UIm 
 
-    return {
-        anotherPublic: function(){
-            console.log(z);
-        }
     }
+    
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
+    document.addEventListener('keypress', function(event){
+        
+        if  (event.keyCode === 13  || event.which === 13){
+            ctrlAddItem(); 
+        }
+
+    });
   })(budgetController,UIController);
